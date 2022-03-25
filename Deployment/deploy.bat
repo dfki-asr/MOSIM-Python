fd9a1c3a8ce645c703cd6322c3ce251e90030869
Janis Sprenger <Janis.Sprenger@dfki.de>
2022-03-25 16:14:55 +0100

    Updated deployment procedure

--- Deployment/deploy.bat
+++ Deployment/deploy.bat
@@ -1,10 +1,13 @@
 @echo off
+@setlocal enabledelayedexpansion
 
 REM SPDX-License-Identifier: MIT
 REM The content of this file has been developed in the context of the MOSIM research project.
 REM Original author(s): Janis Sprenger, Bhuvaneshwaran Ilanthirayan, Klaus Fischer
 
 REM This is a deploy script to auto-generate the components of the MOSIM-CSharp projects and move them to a new environment folder. 
+
+call :CheckPowershell PARENT
 
 SET VERBOSE=0
 
@@ -45,20 +48,35 @@ REM Method Section
 		exit /b 0
 	)
 	
-	if not exist %1 (
-		call :FolderNotFound
-		exit /b 1
-	) else (
-		FOR /F %%i IN ("%1") DO SET "MOSIM_HOME=%%~fi"
-		
-		echo Deploying to: %MOSIM_HOME%
-		SET BUILDENV=%MOSIM_HOME%\Environment
-		SET LIBRARYPATH=%MOSIM_HOME%\Libraries
-		SET REPO=%~dp0\..\
-		
+	SET REPO=%~dp0..\
 
-		SHIFT
+	IF "%1"=="" (
+		ECHO Taking default MOSIM Target Path "%MOSIM_TARGET_PATH%"
+		SET "MOSIM_HOME=%MOSIM_TARGET_PATH%"
+	) ELSE (
+		SET "var=%1"
+		IF "!var:~0,1!"=="-" (
+			ECHO Taking default MOSIM Target Path "%MOSIM_TARGET_PATH%"
+			SET "MOSIM_HOME=%MOSIM_TARGET_PATH%"
+		) ELSE (
+			FOR /F %%i IN ("%1") DO SET "MOSIM_HOME=%%~fi"	
+			ECHO Taking provided MOSIM Target Path at "!MOSIM_HOME!"
+			SHIFT
+		)
 	)
+	
+	if "%MOSIM_HOME%"=="" (
+		ECHO Please provide a target directory 
+		ECHO     - either as a parameter to this script or 
+		ECHO     - adjust the Deployment\DefaultVariables.bat in the meta repository and run Deployment\Initialize.bat
+		ECHO     - or use SETX MOSIM_TARGET_PATH C:\Path\To\My\Target to set the variable manually. 
+		call :halt 1
+	)
+	
+	echo Deploying to: %MOSIM_HOME%
+	SET BUILDENV=%MOSIM_HOME%\Environment
+	SET LIBRARYPATH=%MOSIM_HOME%\Libraries
+	
 	
 	if "%1"=="-v" (
 		ECHO Running in Verbose mode
@@ -146,6 +164,19 @@ if %ERRORLEVEL% NEQ 0 (
 ) else (
 	exit /b
 )
+
+:CheckPowershell
+SET "PSCMD=$ppid=$pid;while($i++ -lt 3 -and ($ppid=(Get-CimInstance Win32_Process -Filter ('ProcessID='+$ppid)).ParentProcessId)) {}; (Get-Process -EA Ignore -ID $ppid).Name"
+
+for /f "tokens=*" %%i in ('powershell -noprofile -command "%PSCMD%"') do SET %1=%%i
+
+IF ["%PARENT%"] == ["powershell"] (
+	ECHO This script should not run from within a Powershell but a Command Prompt aka cmd
+	call :halt 1
+) ELSE (
+    exit /b 1
+)
+
 
 :: Sets the errorlevel and stops the batch immediately
 :halt
